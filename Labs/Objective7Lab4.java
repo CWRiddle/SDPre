c597dd2dc57d12c08611f3e22cf79cced9968691
Chris <cwriddle5@gmail.com>
2021-05-05 15:21:40 -0400

    Objective 7

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,14 @@
+public class Objective7Lab4{
+  public static void main(String[] args){
+    int counter = 1;
+    int num = 0;
+
+    while(counter <= 20)
+    {
+      num += counter;
+      counter++;
+    }
+
+    System.out.println(num);
+  }
+}
