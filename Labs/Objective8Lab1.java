7dd155d05b5b2b8c5f7f7ac3cf7eb41f6231873b
Chris <cwriddle5@gmail.com>
2021-05-05 15:22:19 -0400

    Objective 8

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,12 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+  for(int i = 0; i < 10; i++)
+  {
+    System.out.println(i + 1);
+  }
+  for(int i = 0; i < 10; i++)
+  {
+    System.out.println(10 - i);
+  }
+}
+}
